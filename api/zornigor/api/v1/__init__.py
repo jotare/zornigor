d3f42d079d9ce5486f0a4c6d2f4d44f83d8be75e
Joan Antoni RE <joanantoni.re16@gmail.com>
2023-04-22 16:13:52 +0200

    [api] Move / endpoint to __init__

--- api/zornigor/api/v1/__init__.py
+++ api/zornigor/api/v1/__init__.py
@@ -1,2 +1,12 @@
+from fastapi_versioning import version
+
 from . import story
 from .router import api
+
+
+@api.get("/")
+@version(1)
+async def root():
+    return {
+        "message": "Welcome to Zornigor API v1"
+    }
