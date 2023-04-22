cb25e883a2f044a9ed12a212acf1a8c0e82d5f99
Joan Antoni RE <joanantoni.re16@gmail.com>
2023-04-22 15:53:04 +0200

    [api] Add CORS middleware

--- api/zornigor/app.py
+++ api/zornigor/app.py
@@ -1,12 +1,31 @@
 from typing import Union
+
 from fastapi import FastAPI
 from fastapi_versioning import VersionedFastAPI
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 from starlette.routing import Mount
 
 from zornigor.api.v1.router import api as api_v1
 
 
-base_app = FastAPI(title="Zornigor API")
+middleware = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=[
+            "http://localhost:8081"
+        ],
+        allow_methods=["*"],
+        allow_headers=["*"],
+
+    )
+]
+
+settings = dict(
+    middleware=middleware,
+)
+
+base_app = FastAPI(title="Zornigor API", **settings)
 
 @base_app.get("/")
 async def root():
@@ -32,4 +51,5 @@ app = VersionedFastAPI(
     prefix_format="/api/v{major}",
     default_version=(1, 0),
     enable_latest=True,
+    **settings,
 )
