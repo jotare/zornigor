8b704167bdf7e921e67adb780058d9c241be6f9c
Joan Antoni RE <joanantoni.re16@gmail.com>
2023-04-30 13:10:44 +0200

    [api] Remove useless openapi extension

--- api/zornigor/app.py
+++ api/zornigor/app.py
@@ -12,12 +12,9 @@ from zornigor.api.v1.router import api as api_v1
 middleware = [
     Middleware(
         CORSMiddleware,
-        allow_origins=[
-            "http://localhost:8081"
-        ],
+        allow_origins=["http://localhost:8081"],
         allow_methods=["*"],
         allow_headers=["*"],
-
     )
 ]
 
@@ -27,23 +24,13 @@ settings = dict(
 
 base_app = FastAPI(title="Zornigor API", **settings)
 
+
 @base_app.get("/")
 async def root():
-    return {
-        "message": "Welcome to Zornigor API v1"
-    }
+    return {"message": "Welcome to Zornigor API v1"}
+
 
 base_app.include_router(api_v1)
-
-def extend_openapi(app: Union[FastAPI, Mount]):  # pragma: no cover
-    for route in app.routes:
-        # mypy complains about BaseRoute not having endpoint and
-        # description attributes, but routes passed here always have
-        if hasattr(route.endpoint, "__required_scopes__"):  # type: ignore
-            scopes = route.endpoint.__required_scopes__  # type: ignore
-
-
-extend_openapi(base_app)
 
 app = VersionedFastAPI(
     base_app,
