b787850c6cdeb581338987fce9a3874ec7ae9d31
Joan Antoni RE <joanantoni.re16@gmail.com>
2023-05-14 19:14:15 +0200

    [api] Add create state endpoint

--- api/zornigor/api/v1/state.py
+++ api/zornigor/api/v1/state.py
@@ -6,7 +6,25 @@ from starlette.requests import Request
 from zornigor.api.models.common import Slug
 from zornigor.api.models.state import State
 from zornigor.api.v1.router import PROJECT, STATES, api
+from zornigor.db import models as db_models
 from zornigor.db import states
+
+
+@api.post(
+    f"/{PROJECT}/{{project_id}}/{STATES}",
+    status_code=201,
+    name="Create state",
+    tags=["States"],
+)
+@version(1)
+async def create_state(request: Request, project_id: str, item: State):
+    payload = db_models.State(
+        slug=item.id,
+        name=item.name,
+        description=item.description,
+        project=project_id,
+    )
+    await states.create_state(payload)
 
 
 @api.get(
